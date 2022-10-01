c7afce624d1710cdf4e5af53b80feb9b961ed6d6
Vladimir Romanov <valgard5@mail.ru>
2022-10-01 14:38:47 +0300

    initial commit

---
+++ Homework3.6.1/Header.h
@@ -0,0 +1,6 @@
+#pragma once
+double summary(double num_a, double num_b);
+double substract(double num_a, double num_b);
+double mult(double num_a, double num_b);
+double div(double num_a, double num_b);
+double exp(double num_a, double num_b);
---
+++ Homework3.6.1/Source.cpp
@@ -0,0 +1,22 @@
+#include "Header.h"
+#include <cmath>
+double summary(double num_a, double num_b)
+{
+	return num_a + num_b;
+};
+double substract(double num_a, double num_b)
+{
+	return num_a - num_b;
+};
+double mult(double num_a, double num_b)
+{
+	return num_a * num_b;
+};
+double div(double num_a, double num_b)
+{
+	return num_a / num_b;
+;};
+double exp(double num_a, double num_b)
+{
+	return pow(num_a, num_b);
+};
